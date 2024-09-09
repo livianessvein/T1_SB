a1463c397cfa81c5905bb7cde5a76c78e4b78da1
livianessvein <livianessvein.puc@gmail.com>
2024-09-09 15:27:05 -0300

    Primeiros dados

---
+++ converteutf832.c
@@ -0,0 +1,7 @@
+// Você deve criar um arquivo fonte chamado converteutf832.c contendo as duas funções descritas acima, e funções auxiliares, se for o caso. Esse arquivo não deve conter uma função main!
+
+#include <stdio.h>
+#include "converteutf832.h"
+
+int convUtf8p32(FILE *arquivo_entrada, FILE *arquivo_saida);
+int convUtf32p8(FILE *arquivo_entrada, FILE *arquivo_saida);
---
+++ testeconv.c
@@ -0,0 +1,1 @@
+#include "converteutf832.h"
